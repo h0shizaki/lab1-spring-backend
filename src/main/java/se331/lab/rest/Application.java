67bc064a7db9295c47012754d3f7baeaf101098e
KAN KATPARK <kant.cmd@gmail.com>
2023-10-08 04:29:21 +0700

    (patch): add origin

--- src/main/java/se331/lab/rest/Application.java
+++ src/main/java/se331/lab/rest/Application.java
@@ -21,6 +21,7 @@ public class Application {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://34.238.12.28/")
                         .exposedHeaders("x-total-count");
             }
         };
